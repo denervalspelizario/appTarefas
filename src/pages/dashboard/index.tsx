86c89da843eee2a464cb371d78e5247147e73578
denervalspelizario <denervalspelizario@gmail.com>
2023-04-11 10:31:12 -0300

    update apptarefas criando a estrutura de taskContainer ja com sua estilizacao

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,6 +5,9 @@ import Head from 'next/head' // esse head é o texto que fica na aba da pagina
 import { getSession } from 'next-auth/react' // importando o getSession para verificar se a sessão ativa
 import { Textarea } from '../../components/textArea'
 
+import { FiShare2 } from 'react-icons/fi'
+import { FaTrash } from 'react-icons/fa'
+// comando para instalar icons yarn add react-icons
 
 export default function Dashboard(){
   return(
@@ -35,9 +38,31 @@ export default function Dashboard(){
 
           </div>
         </section>
-      </main>
-      
 
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+          <article className={styles.task}>
+            
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>
+                PUBLICO
+              </label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color='#3183FF'/>
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa de exempo show demais</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color='#ea3140' />
+              </button>
+            </div>
+
+          </article>
+        </section>
+      
+      </main>
     </div>
   )
 }
