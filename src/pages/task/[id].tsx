2404a5ff15be2c47bbf252e206179e0e9cba0049
denervalspelizario <denervalspelizario@gmail.com>
2023-04-21 11:04:34 -0300

    update appTarefas criando em task uma collection nova que cria no databse todos so comenatrios do user  e depois adiciona essa collecton no caso comment dentro de um array chamado allComments para ser usada como props

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -11,6 +11,7 @@ import {
   collection,
   query, 
   addDoc,
+  getDocs,
   where // para filtrar
 } from 'firebase/firestore'
 
@@ -32,9 +33,19 @@ interface TaskProps { // criando a tipagem de  item( obejto que formata dados da
     user: string,
     taskId: string
   }
+  allComments: CommentProps
 }
 
-export default function Task({ item }: TaskProps){  // recendo item(objeto com dados da tarefa formatado) la da funcao abaixo getServerSideProps
+interface CommentProps { // criando a tipagem de  allComments
+    id: string,
+    comment: string,
+    user: string,
+    name: string,
+    taskId: string
+}
+
+
+export default function Task({ item, allComments }: TaskProps){  // recendo item(objeto com dados da tarefa formatado) la da funcao abaixo getServerSideProps
 
   const { data: session  } = useSession() ; // data = dados firebase    session = true/logado  false/deslogado
   /*O useSession é um hook fornecido pela biblioteca Next.js que permite acessar informações de sessão do usuário em páginas ou componentes React.
@@ -44,7 +55,8 @@ export default function Task({ item }: TaskProps){  // recendo item(objeto com d
 
   const [input, setInput] = useState("")  
 
-
+  //FUNCAO QUE AO USER CLICAR NO BOTÃO FO FORMULÁRIO VERIFICA SE USER ESTA LOGADO SE ESTIVER CRIAR UMA NOVA COLLECTION NO FIREBASE COM A SEGUINTE ESTRUTURA
+  // COMMENT(COMENTARIO NO INPUT) - CREATED(DATA QUE FOI CRIADO O COMENTARIO) - USER(NOME EMAIL) - NAME(NOME USUARIO) - ID(ID DO COMMETS CRIADO)
   async function tratarComentarios(event: FormEvent){ // O objeto FormEvent é utilizado para representar eventos que ocorrem em um formulário HTML.
     event.preventDefault(); // prevenindo que não se atualize sonho
 
@@ -56,21 +68,20 @@ export default function Task({ item }: TaskProps){  // recendo item(objeto com d
       return;
     }
 
-    try{ // deu certo
-      const docRef = await addDoc(collection(db, "comments"), {
-        comment: input,
+    try{ // deu certo após passar pelas condicionais acima  cria uma NOVA collection la no firebase chamada comments
+      const docRef = await addDoc(collection(db, "comments"), { // criando um doc no formato collection no db(firebase) com nome de commets
+        comment: input,      // este collection terá este dados
         created: new Date(),
         user: session?.user?.email,
         name: session?.user?.name,
         taskId: item?.taskId
       }) 
 
-      setInput("") // esvaziando o input  
+      setInput("") // após criar a collection será esvaziando o input  
     
     } catch(err){ // deu erro
       console.log(err) // clg do erro
     }
-
   }
 
   return(
@@ -92,9 +103,15 @@ export default function Task({ item }: TaskProps){  // recendo item(objeto com d
           <Textarea 
             value={input} // seu valor inicial sera state input
             placeholder='digite seu comentário....'
-            onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setInput(event.target.value)} 
+            onChange={
+              (event: ChangeEvent<HTMLTextAreaElement>) => // pegando oque foi digitado(event) e jogando no input
+               setInput(event.target.value)
+              } 
           />
-          <button className={styles.button} disabled={!session?.user}>
+          <button 
+            className={styles.button} 
+            disabled={!session?.user} // se user estiver logado ou seja session.user estiver true disble será true senão false e botão será desabilitado
+          >
             Enviar comentário
           </button>
         </form>
@@ -114,6 +131,24 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {  /
   const id = params?.id as string;  // garantindo e ja tipando que este id da tarefa será uma string
 
   const docRef = doc(db, "tarefas", id) // docRef é referenciando por db > tarefas > id  para poder chegar na id exata da tarefa ou seja pega um doc especifico
+
+
+  //TODOS AS ARRAY DE COMENTARIOS DO USUARIO(olhar q la tem o filtro) SERÁO ADICIONANDO DENTRO DE ALLCOMMENTS
+  // OU SEJA EM ALLCOMMENTS CONSTARÁ TODOS OS COMENTARIOS DO USUÁRIO
+  const q = query(collection(db, "comments"), where("taskId", "==", id)) // filtrando apenas comments do db tendo o id seja igual a id logada e jogando na const q   
+  const snapshotComments = await getDocs(q) // Depois de filtrar adicionando esses dados de comments do user para snapshotComments
+  let allComments: CommentProps[] = [] // estabelecendo tipagem definida pela interface CommentProps[]
+  snapshotComments.forEach((doc) => { // jogando todos os comments de usuariod entro de allComments
+    allComments.push({
+      id: doc.id,
+      comment: doc.data().comment,
+      user: doc.data().user,
+      name: doc.data().name,
+      taskId: doc.data().taskId
+    })
+  })
+
+  console.log(allComments)
 
   const snapshot = await getDoc(docRef) // repassando o doc para o snapshot
 
@@ -149,10 +184,11 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {  /
   }
   console.log(task)
 
-  // RETORNANDO A TAREFA TODA FORMATADA
+  // RETORNANDO A TAREFA TODA FORMATADA E TODOS OS COMENTARIOS DO USER LOGADO
   return {
     props: {
-      item: task
+      item: task,
+      allComments: allComments,
     }
   }
 }
